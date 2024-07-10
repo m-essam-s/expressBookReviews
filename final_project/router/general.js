a3079446c666f5511449de60fc837c7d44e62aca
m-essam-s <sir.m.essam.g@gmail.com>
2024-07-10 19:34:08 +0300

    feat: Implement user registration route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,16 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.query;
+  if (!username || !password) {
+      return res.status(400).json({ message: "Username and password required" });
+  }
+  const userExists = users.find(user => user.username === username);
+  if (userExists) {
+      return res.status(400).json({ message: "Username already exists" });
+  }
+  users.push({ username, password });
+  return res.status(200).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
