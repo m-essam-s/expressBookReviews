c28081bea39729446f647978e6167f3409391484
m-essam-s <sir.m.essam.g@gmail.com>
2024-07-10 18:10:14 +0300

    feat: Add route to get book details by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,18 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const results = [];
+  for (const bookId in books) {
+    if (books[bookId].author === author) {
+      results.push(books[bookId]);
+    }
+  }
+  if (results.length > 0) {
+    return res.status(200).json(results);
+  } else {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
 });
 
 // Get all books based on title
