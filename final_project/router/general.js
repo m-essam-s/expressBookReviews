a3265d6827326bf677b723b1c83436046679806a
m-essam-s <sir.m.essam.g@gmail.com>
2024-07-10 20:28:40 +0300

    refactor: Use async/await to get list of all books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,18 +19,26 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  return res.status(200).json(books);
+public_users.get('/', async (req, res) => {
+  try {
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving books" });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res) => {
   const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
-    return res.status(200).json(book);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
+  try {
+    const book = books[isbn];
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving book by ISBN" });
   }
 });
 
