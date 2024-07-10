7c030aa5c16f15c3d1f2d41476aa7958caacd01d
m-essam-s <sir.m.essam.g@gmail.com>
2024-07-10 18:53:14 +0300

    feat: Add route to get book details by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,8 +44,18 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const results = [];
+  for (const bookId in books) {
+    if (books[bookId].title === title) {
+      results.push(books[bookId]);
+    }
+  }
+  if (results.length > 0) {
+    return res.status(200).json(results);
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
